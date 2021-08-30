c88108d43d1aaf1c845a4c80d37115a328f444a6
luzmartinez23 <romi254970@gmail.com>
2021-08-30 12:12:36 -0600

    prueba

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+
+    return "Bienvenido a la pagina principal";
+});
+Route::get('cursos',function(){
+    return "Bienvenido a la pagina cursos ";
+});
+Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+    
+});
+
+/*Route::get('cursos/{curso}',function($curso){
+    return "Bienvenido al curso: $curso ";
+}); */
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria=null) {
+    if($categoria){
+    return "Bienvenido al curso $curso, de la categoria $categoria";
+    }else{
+        return "Bienvenido al curso: $curso ";
+    }
 });
